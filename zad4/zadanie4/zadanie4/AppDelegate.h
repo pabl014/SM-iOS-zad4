6f86b25564aa2183e74bd832a5662a5777583b1d
Paweł Rudnik <126992888+pabl014@users.noreply.github.com>
2023-11-04 18:48:47 +0100

    Add files via upload

---
+++ zad4/zadanie4/zadanie4/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  zadanie4
+//
+//  Created by Paweł Rudnik on 04/11/2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
